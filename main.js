c2046aaa53fb8a234d0e72ec3e2725c057e8c03f
Eliezer Peña <147127744+mrZabala@users.noreply.github.com>
2024-03-08 18:58:42 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,85 @@
+const buttonNumbers = document.getElementsByName('data-number');
+const buttonOperands = document.getElementsByName('data-opera');
+const buttonResult = document.getElementsByName('data-result')[0];
+const buttonDelete = document.getElementsByName('data-delete')[0];
+var output = document.getElementById('output');
+var opeCurrent = '';
+var opePrevious = '';
+var operation = undefined;
+
+buttonNumbers.forEach(function(boton){
+    boton.addEventListener('click', function(){
+        aggNumber(boton.innerText);
+    })
+});
+
+buttonOperands.forEach(function(boton){
+    boton.addEventListener('click', function(){
+        selectOperation(boton.innerText);
+    })
+});
+
+buttonResult.addEventListener('click', function(){
+    calculate();
+    updateDisplay(); 
+});
+
+buttonDelete.addEventListener('click', function(){
+    clear();
+    updateDisplay(); 
+});
+
+function aggNumber(number){
+    opeCurrent = opeCurrent.toString() + number.toString();
+    updateDisplay();
+}
+
+function updateDisplay(){
+    output.value = opeCurrent;
+}
+
+function clear(){
+     opeCurrent = '';
+     opePrevious = '';
+     operation = undefined;
+}
+
+function calculate(){
+    var calc;
+    const previus = parseFloat(opePrevious);  // Corregí el nombre de la variable a previus
+    const current = parseFloat(opeCurrent);
+    if(isNaN(previus) || isNaN(current)) return;
+    switch(operation){
+        case '+':
+            calc = previus + current;
+            break;
+        case '-':
+            calc = previus - current;
+            break;
+        case '/':
+            calc = previus / current;
+            break;
+        case '*':
+            calc = previus * current;
+            break;
+        default:
+            return;
+    }
+
+    opeCurrent = calc.toString();
+    operation = undefined;
+    opePrevious = '';
+}
+
+function selectOperation(op){
+    if(opeCurrent === '') return;
+    if(opePrevious !== ''){
+        calculate();
+    }
+
+    operation = op.toString();
+    opePrevious = opeCurrent;
+    opeCurrent = '';
+}
+
+clear();
